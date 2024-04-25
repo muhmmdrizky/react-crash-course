84841bd182f9d380d090240fd1336a15c8515e12
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-04-25 10:47:17 +0700

    init

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -32,7 +32,7 @@ const JobListing = ({ job }) => {
             <FaMapMarker className="inline text-lg mr-1 mb-1" />
             {job.location}
           </div>
-          <Link to={`/job/${job.id}`} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+          <Link to={`/jobs/${job.id}`} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
             Read More
           </Link>
         </div>
