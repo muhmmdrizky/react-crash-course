94d573c0441f5c2c769c8d0af3292a1d1b459915
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-04-26 14:44:54 +0700

    init

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { useParams, useLoaderData } from "react-router-dom";
-import { FaArrowLeft, FaMapMarker, FaMarker } from "react-icons/fa";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm("Are you sure want to delete this listing?");
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success("Job successfully deleted");
+
+    navigate("/jobs");
+  };
 
   return (
     <>
@@ -62,10 +76,12 @@ const JobPage = () => {
 
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                <Link to={`/jobs/edit/${job.id}`} className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <Link to={`/edit-job/${job.id}`} className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">Delete Job</button>
+                <button onClick={() => onDeleteClick(job.id)} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                  Delete Job
+                </button>
               </div>
             </aside>
           </div>
